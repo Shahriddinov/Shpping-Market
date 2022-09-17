10299ddd7ebcb3beb9b600fdb2437c352b7fd382
UnknownCoderMaster <110689681+UnknownCoderMaster@users.noreply.github.com>
2022-09-17 20:32:14 +0500

    Orzibek Components ga qo'shimcha qo'shib qo'ydim!

---
+++ src/components/NextButton/next.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
+import "./style.scss";
+import CancelOutlinedIcon from "@mui/material/SvgIcon/SvgIcon";
+
+export default function Next() {
+    return (
+        <Stack spacing={2} direction="row">
+            <Button className="button" style={{backgroundColor: "#0FBE7B"}} variant="contained"> <span className="icon"><CheckCircleOutlineOutlinedIcon fontSize="small"/></span> Продолжить</Button>
+        </Stack>
+    );
+}
